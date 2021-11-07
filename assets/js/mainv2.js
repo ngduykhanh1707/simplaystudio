2827a6f8b36a033cde99bb0216b3a8852f115e31
Khanh Nguyen <nguyenduykhanh.it.bt@gmail.com>
2021-11-07 14:11:43 +0700

    no message

---
+++ assets/js/mainv2.js
@@ -0,0 +1,183 @@
+$('header').append("<div class='inner'><a href='index.html' class='logo'><span class='symbol'><img src='images/logo.png' alt='' /></span><span class='title'>Blue DOT Game</span></a><nav><ul><li><a href='#menu'>Menu</a></li></ul></nav></div>");
+//$('footer').append("<div class='inner'><section><h2>Get in touch</h2><form method='post' action='#'><div class='fields'><div class='field half'><input type='text' name='name' id='name' placeholder='Name' /></div><div class='field half'><input type='email' name='email' id='email' placeholder='Email' /></div><div class='field'><textarea name='message' id='message' placeholder='Message'></textarea></div></div><ul class='actions'><li><input type='submit' value='Send' class='primary' /></li></ul></form></section><section><h2>Follow</h2><ul class='icons'><li><a href='https://www.facebook.com/bluedotgame/' class='icon style2 fa-facebook'><span class='label'>Facebook</span></a></li><li><a href='https://www.instagram.com/bluedotgame/' class='icon style2 fa-instagram'><span class='label'>Instagram</span></a></li></ul></section><ul class='copyright'><li>&copy; Blue DOT Game. All rights reserved</li></ul></div>");
+$('footer').append("<div class='inner'><section><h2>Get in touch</h2><ul class='icons'><li><a href='mailto:bluedotsofts@gmail.com' class='icon style2 fa-envelope'><span class='label'>Facebook</span></a></li><li><a href='https://www.facebook.com/bluedotgame/' class='icon style2 fa-facebook'><span class='label'>Facebook</span></a></li><li><a href='https://www.instagram.com/bluedotgame/' class='icon style2 fa-instagram'><span class='label'>Instagram</span></a></li></ul></section><ul class='copyright'><li>&copy; Blue DOT Game. All rights reserved</li></ul></div>");
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('is-touch');
+
+	// Forms.
+		var $form = $('form');
+
+		// Auto-resizing textareas.
+			$form.find('textarea').each(function() {
+
+				var $this = $(this),
+					$wrapper = $('<div class="textarea-wrapper"></div>'),
+					$submits = $this.find('input[type="submit"]');
+
+				$this
+					.wrap($wrapper)
+					.attr('rows', 1)
+					.css('overflow', 'hidden')
+					.css('resize', 'none')
+					.on('keydown', function(event) {
+
+						if (event.keyCode == 13
+						&&	event.ctrlKey) {
+
+							event.preventDefault();
+							event.stopPropagation();
+
+							$(this).blur();
+
+						}
+
+					})
+					.on('blur focus', function() {
+						$this.val($.trim($this.val()));
+					})
+					.on('input blur focus --init', function() {
+
+						$wrapper
+							.css('height', $this.height());
+
+						$this
+							.css('height', 'auto')
+							.css('height', $this.prop('scrollHeight') + 'px');
+
+					})
+					.on('keyup', function(event) {
+
+						if (event.keyCode == 9)
+							$this
+								.select();
+
+					})
+					.triggerHandler('--init');
+
+				// Fix.
+					if (browser.name == 'ie'
+					||	browser.mobile)
+						$this
+							.css('max-height', '10em')
+							.css('overflow-y', 'auto');
+
+			});
+
+	// Menu.
+		var $menu = $('#menu');
+
+		$menu.wrapInner('<div class="inner"></div>');
+
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+
+				var href = $(this).attr('href');
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Hide.
+					$menu._hide();
+
+				// Redirect.
+					if (href == '#menu')
+						return;
+
+					window.setTimeout(function() {
+						window.location.href = href;
+					}, 350);
+
+			})
+			.append('<a class="close" href="#menu">Close</a>');
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('click', function(event) {
+
+				// Hide.
+					$menu._hide();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+			});
+
+})(jQuery);
